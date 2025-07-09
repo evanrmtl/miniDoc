f450d7b240de2ef3d69b2305a8952e771f6b36d9
minipoucetm <evanrimonteil2@gmail.com>
2025-07-10 00:48:20 +0200

    update DB

--- minidoc_database.sql
+++ minidoc_database.sql
@@ -8,13 +8,17 @@ CREATE TABLE files (
     file_id INTEGER PRIMARY KEY AUTOINCREMENT,
     file_name TEXT NOT NULL,
     file_content TEXT,
-    file_updated_at INT NOT NULL 
+    file_updated_at INT NOT NULL,
+    owner_id INTEGER NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE users_files (
     user_id INTEGER NOT NULL,
     file_id INTEGER NOT NULL,
+    role TEXT NOT NULL DEFAULT 'collaborator',
     PRIMARY KEY (user_id, file_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (file_id) REFERENCES files(file_id) ON DELETE CASCADE
+    FOREIGN KEY (file_id) REFERENCES files(file_id) ON DELETE CASCADE,
+    CHECK (role IN ('owner', 'collaborator'))
 );
