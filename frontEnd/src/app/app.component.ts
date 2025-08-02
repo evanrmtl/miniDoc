bab6f5a0476931e46005c9353874dade981bf836
minipoucetm <evanrimonteil2@gmail.com>
2025-08-02 02:56:06 +0200

    feature: UI for login/register

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,24 +1,53 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UiService } from './services/UI/UiService.service';
+import { NotificationService } from './services/notification/notification.service';
+import { RefreshService } from './services/refresh/refreash.service';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'miniDoc';
 
-  constructor(private cdRef: ChangeDetectorRef, private uiService: UiService){}
+  private notification: { message: string, type: string } | null = null;
+
+  constructor(
+    private refreshService: RefreshService,
+    private cdRef: ChangeDetectorRef, 
+    private uiService: UiService, 
+    private notificationService: NotificationService
+  ) {
+    this.notificationService.notification.subscribe(msg => {
+      this.notification = msg; 
+      setTimeout(() => this.notification = null, 2000);
+    });
+
+    effect(() => {
+      if (this.refreshService.refreshSignal()) {
+        this.reload();
+        this.refreshService.clearRefresh()
+      }
+    });
+  }
 
   ngAfterViewInit() {
     this.cdRef.detectChanges();
   }
 
+  get getNotification(): { message: string, type: string } | null {
+    return this.notification;
+  }
+
   get isModalOpen(): boolean {
     return this.uiService.isModalOpen;
+  }
+
+  reload(){
+    this.uiService.closeLoginModal()
   }
 }
